bd2f7dd55e6d227069f99e962e8ed0e4ebf3e7ad
BIW <biw@BIWs-Mac-mini.local>
2026-07-02 15:40:41 +0600

    fix: downgrade tailwind to v3 to fix vercel build

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,18 +1,16 @@
-import type { Config } from "tailwindcss";
-
-const config: Config = {
+/** @type {import('tailwindcss').Config} */
+module.exports = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   corePlugins: {
-    // Disable preflight so it doesn't break their existing vanilla CSS headers/layouts
     preflight: false,
   },
   theme: {
     extend: {},
   },
   plugins: [],
-};
-export default config;
+}
+
